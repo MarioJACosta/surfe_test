9ed1cf412481264f2f86060ced818f500d0761f7
Mario Costa <mario.costa.13@gmail.com>
2025-03-26 07:54:05 +0000

    Task: load users and actions data

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"surfe_test/models"
+)
+
+func main(){
+	users, actions, err := loadData()
+	if err != nil{
+		log.Fatalf("couldn't load data: %s", err.Error())
+	}
+
+	fmt.Println(users)
+	fmt.Println(actions)
+}
+
+func loadData() ([]models.User, []models.Action, error){
+	var (
+		users []models.User
+		actions []models.Action
+	)
+
+	usersData, err  := os.ReadFile("data/users.json")
+	if err != nil{
+		return nil, nil, fmt.Errorf("couldn't load users data: %s", err.Error())
+	}
+
+	if err = json.Unmarshal(usersData, &users); err != nil{
+		return nil, nil, fmt.Errorf("couldn't unmarshall users data: %s", err. Error())
+	}
+
+	
+	actionsData, err  := os.ReadFile("data/actions.json")
+	if err != nil{
+		return nil, nil, fmt.Errorf("couldn't load actions data: %s", err.Error())
+	}
+
+	if err = json.Unmarshal(actionsData, &actions); err != nil{
+		return nil, nil, fmt.Errorf("couldn't unmarshall actions data: %s", err. Error())
+	}
+
+	return users, actions, nil
+
+}
---
+++ models/action.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Action struct {
+	ID int `json:"id"`
+	Type string `json:"type"`
+	UserID int `json:"userId"`
+	TargetUser int `json:"targetUser"`
+	CreatedAt time.Time `json:"createdAt"`
+}
